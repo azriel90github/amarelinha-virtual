0d5c363161be3a50a2b6ed353b01971a6edc3557
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2025-02-25 11:24:57 +0100

    geral update

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -7,74 +7,11 @@ async function seed() {
 
 	await db.insert(goods).values([
 		{
-			title: "Morro",
-			price: "310", // Agora pode ser um number
+			title: "Hambuguer tuneza simples de carne",
+			price: "40000", // Agora pode ser um number
 			heart: 2,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Menta"
-		},
-		{
-			title: "Rio de Janeiro",
-			price: "320", // Agora pode ser um number
-			heart: 3,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Menta"
-		},
-		{
-			title: "Canada",
-			price: "330", // Agora pode ser um number
-			heart: 4,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Chocolate"
-		},
-		{
-			title: "Miami",
-			price: "370", // Agora pode ser um number
-			heart: 3,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Chocolate"
-		},
-		{
-			title: "Luxenburgo",
-			price: "340", // Agora pode ser um number
-			heart: 3,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Banana"
-		},
-		{
-			title: "Havai",
-			price: "380", // Agora pode ser um number
-			heart: 3,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Banana"
-		},
-		{
-			title: "Florianopólis",
-			price: "440", // Agora pode ser um number
-			heart: 1,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Água"
-		},
-		{
-			title: "Dubai",
-			price: "450", // Agora pode ser um number
-			heart: 1,
-			description: "Mistura de morango com chocolate, bolachas e uma cereginha",
-			category: "Água"
-		},
-		{
-			title: "Angola",
-			price: "540", // Agora pode ser um number
-			heart: 4,
-			description: "Mistura de morango com chocolate, bolachas e uma caneca de café",
-			category: "Baunilha"
-		},
-		{
-			title: "Malanje",
-			price: "740", // Agora pode ser um number
-			heart: 4,
-			description: "Mistura de morango com chocolate, bolachas e uma caneca de café",
-			category: "Baunilha"
+			description: "hambúrguer tuneza ( carne + ovo + bacon + queijo cheddar + batata palha + molho especial da casa )",
+			category: "Tuneza"
 		},
 	]);
 
